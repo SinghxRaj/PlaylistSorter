e15aa0f876440b1e2e04853392359147960a303f
RickySin11 <rajvir11singh@outlook.com>
2022-03-24 12:36:52 -0700

    Create function to get access token
    * Created a function that gets the access token for the Spotify API so
      that requests can be made to the Spotify API.
    
    * Wrote documentation for the routes in the auth module and for the
      functions in PlaylistSorter/auth/util.py.

--- PlaylistSorter/auth/routes.py
+++ PlaylistSorter/auth/routes.py
@@ -1,17 +1,24 @@
 from flask import redirect, request, session, url_for, Blueprint
-import spotipy
 from PlaylistSorter.auth.util import create_spotify_oauth
 
 auth_blueprint = Blueprint('auth', __name__)
 
 @auth_blueprint.route('/login')
 def login():
+  """
+  Redirects the user to the Spotify login page to authorize
+  permissions
+  """
   spotify_oauth = create_spotify_oauth()
   auth_url = spotify_oauth.get_authorize_url()
   return redirect(auth_url)
 
 @auth_blueprint.route('/callback')
 def callback():
+  """
+  The user to redirected to this route after they finish logging
+  in
+  """
   spotify_oauth = create_spotify_oauth()
   session.clear()
   code = request.args.get('code')
--- PlaylistSorter/auth/util.py
+++ PlaylistSorter/auth/util.py
@@ -1,11 +1,37 @@
 import os
 from spotipy.oauth2 import SpotifyOAuth
-from flask import url_for
+from flask import url_for, session
 
-def create_spotify_oauth():
+def create_spotify_oauth() -> SpotifyOAuth:
+  """
+  Returns an instance of the SpotifyOAuth that is configured for
+  the application
+  """
   return SpotifyOAuth(
     client_id=os.environ.get('SPOTIFY_CLIENT_ID'),
     client_secret=os.environ.get('SPOTIFY_CLIENT_SECRET'),
     redirect_uri=url_for('auth.callback', _external=True),
     scope='playlist-modify-private playlist-read-private playlist-modify-public user-read-recently-played user-top-read'
   )
+
+def get_token() -> dict:
+  """
+  Returns the access token
+
+  If the current access token is expired a new one will be created
+  and returned
+
+  Raises
+  -----------
+  Exception
+  If there doesn't exist an access token in the current session
+  """
+  token_info = session.get("token_info", None)
+  if not token_info:
+    raise "exception"
+  spotify_oauth = create_spotify_oauth()
+  token_info = session['token_info']
+  if(spotify_oauth.is_token_expired(token_info)):
+    token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
+    session['token_info'] = token_info
+  return token_info
